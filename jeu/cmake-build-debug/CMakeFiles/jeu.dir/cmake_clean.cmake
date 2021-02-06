16a7b09e12599e74a84f6122b4a05e0f4f73ab4a
jbarber07 <jeremy.barber@ulb.be>
2021-02-06 09:26:57 +0100

    Game init commit

---
+++ jeu/cmake-build-debug/CMakeFiles/jeu.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jeu.dir/main.cpp.o"
+  "jeu"
+  "jeu.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jeu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ jeu/cmake-build-debug/CMakeFiles/jeu.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for jeu.
+# This may be replaced when dependencies are built.
