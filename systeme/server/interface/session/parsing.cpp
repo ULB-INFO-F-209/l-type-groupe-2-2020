89ebf16adcb606f286b04fa8713099292b5b9cbd
koadegno <vinove9@hotmail.fr>
2021-02-13 15:37:22 +0100

    [CLASS PARSING] [* TO TEST*] decodage des structures en string

--- systeme/server/interface/session/parsing.cpp
+++ systeme/server/interface/session/parsing.cpp
@@ -33,7 +33,73 @@ void Parsing::create_game_to_str(char *buffer, Game_settings * settings){
 }
 
 //decodage
-void Parsing::leaderboard_from_str(char *buffer,std::vector<Profile*> prof){}
-void Parsing::pseudo_list_from_str(char *buffer, std::vector<char*> list){}
-void Parsing::profile_from_str(char *buffer, Profile *prof){}
-void Parsing::create_game_from_str(char *buffer, Game_settings * settings){}
+void Parsing::leaderboard_from_str(char *buffer,std::vector<Profile*> prof){
+	std::string cpp_str_buffer(buffer);
+	const char delimiteur_score = '&';
+	const char delimiteur_nom = '|';
+	Profile * Player_profile=nullptr;
+
+	// TOTEST !!!
+	while(std::size_t index = cpp_str_buffer.find(delimiteur_nom) != cpp_str_buffer.npos){ // pas sur pour le npos
+		std::string name_and_score = cpp_str_buffer.substr(0,index);
+		cpp_str_buffer = cpp_str_buffer.substr(index,cpp_str_buffer.size());
+
+		index = name_and_score.find(delimiteur_score);
+		Player_profile->pseudo = (name_and_score.substr(0,index)).c_str();
+		Player_profile->score = atoi((name_and_score.substr(index+1,name_and_score.size()).c_str()));
+		prof.push_back(Player_profile);
+	}
+
+}
+void Parsing::pseudo_list_from_str(char *buffer, std::vector<char*> list){
+	// buffer -> "pseudo1&pseudo2&pseudo3&pseudo4..."
+	std::string cpp_str_buffer(buffer);
+	const char delimiteur_nom = '&';
+	while(std::size_t index = cpp_str_buffer.find(delimiteur_nom) != cpp_str_buffer.npos){
+		std::string name = cpp_str_buffer.substr(0,index);
+		cpp_str_buffer = cpp_str_buffer.substr(index,cpp_str_buffer.size());
+		list.push_back(name);
+	}
+	
+}
+void Parsing::profile_from_str(char *buffer, Profile *prof){
+	//
+
+}
+void Parsing::create_game_from_str(char *buffer, Game_settings * settings){
+	//buffer -> "nb_player&pseudo_hote&other_pseudo&drop_rate&ally_shot&nb_lives"
+	std::string cpp_str_buffer(buffer);
+	const char delimiteur_score = '&';
+	std::size_t index = cpp_str_buffer.find(delimiteur_nom);
+
+	//nb player
+	std::string option = cpp_str_buffer.substr(0,index);
+	cpp_str_buffer = cpp_str_buffer.substr(index,cpp_str_buffer.size());
+	settings->nb_lives = atoi(option);
+
+	//pseudo_hote
+	option = cpp_str_buffer.substr(0,index);
+	cpp_str_buffer = cpp_str_buffer.substr(index,cpp_str_buffer.size());
+	settings->nb_lives =option.c_str();
+
+	//pseudo_other
+	option = cpp_str_buffer.substr(0,index);
+	cpp_str_buffer = cpp_str_buffer.substr(index,cpp_str_buffer.size());
+	settings->nb_lives =option.c_str();
+
+	//drop_rate
+	option = cpp_str_buffer.substr(0,index);
+	cpp_str_buffer = cpp_str_buffer.substr(index,cpp_str_buffer.size());
+	settings->nb_lives = atoi(option); // TODO drop_rate est un float trouvé un equivalente de atoi pour float
+
+	//ally_shot
+	option = cpp_str_buffer.substr(0,index);
+	cpp_str_buffer = cpp_str_buffer.substr(index,cpp_str_buffer.size());
+	settings->nb_lives = atoi(option); //TODO changer le false en un nb 0 ou 1 plus simple pour la convertion
+
+	//nb_lives
+	option = cpp_str_buffer.substr(0,index);
+	cpp_str_buffer = cpp_str_buffer.substr(index,cpp_str_buffer.size());
+	settings->nb_lives = atoi(option);
+
+}
--- systeme/server/interface/session/parsing.hpp
+++ systeme/server/interface/session/parsing.hpp
@@ -9,6 +9,7 @@
 
 #include <vector>
 #include <iostream>
+#include <string>
 
 struct Profile
 {
