08d37473518ad534f0bd89f2500caa4fee8d50a4
jbarber07 <jeremy.barber@ulb.be>
2021-02-19 16:55:05 +0100

    save

---
+++ game/src/InputHandler.h
@@ -0,0 +1,14 @@
+//
+// Created by jean on 19/02/2021.
+//
+
+#ifndef GAME_INPUTHANDLER_H
+#define GAME_INPUTHANDLER_H
+
+
+class InputHandler {
+
+};
+
+
+#endif //GAME_INPUTHANDLER_H
---
+++ game/src/Interface.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jean on 19/02/2021.
+//
+
+#include "Interface.h"
