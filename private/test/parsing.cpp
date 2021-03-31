3e4cf443ab20831c7cb0c7b4233fa463892e37bf
koadegno <vinove9@hotmail.fr>
2021-03-31 23:19:45 +0200

    [PARSING] level editor

--- private/test/parsing.cpp
+++ private/test/parsing.cpp
@@ -34,14 +34,30 @@ void Parsing::create_game_to_str(char *buffer, Game_settings * settings){
 	//buffer -> "nb_player&pseudo_hote&other_pseudo&drop_rate&ally_shot&nb_lives"
 }
 
+void Parsing::level_editor_to_str(char * buffer, editor_settings* level_editor){
+	/*
+	L_pseudo_levelName_enemyNumber_enemyHp_enemyDamage_enemySpeed_damageUp_tripleShot_lifeSteal
+	_minigun_dropRate_obstacleNumber_obstacleHp_obstacleDamage_obstacleSpeed
+	_p1Damage_p1Lives_p1ShipDesign_p2Damage_p2Lives_p2ShipDesign
+	*/
+	
+	sprintf(buffer, "L_%s_%s_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d_%d&",
+		level_editor->_pseudo, level_editor->_level_name,level_editor->enemy_number, level_editor->enemy_hp,
+		level_editor->enemy_damage,level_editor->enemy_speed,level_editor->damage_up,level_editor->triple_shot,
+		level_editor->life_steal,level_editor->minigun,level_editor->drop_rate,level_editor->obstacle_number,
+		level_editor->obstacle_hp,level_editor->obstacle_damage,level_editor->obstacle_speed,level_editor->p1_damage,
+		level_editor->p1_lives,level_editor->p1_ship_design,level_editor->p2_damage,level_editor->p2_lives,level_editor->p2_ship_design);
+
+}
+
+
 //decodage
 void Parsing::profile_list_from_str(char *buffer,std::vector<Profile> *prof){
 	std::string cpp_str_buffer(buffer);
 	constexpr char delimiteur_score = '&';
 	constexpr char delimiteur_nom = '|';
 
-	// TO  TEST !!! : merci
-	while(cpp_str_buffer.size() > 1){ // pas sur pour le npos
+	while(cpp_str_buffer.size() > 1){ 
 		std::size_t index = cpp_str_buffer.find(delimiteur_score);
 		std::string name_and_score = cpp_str_buffer.substr(0,index);
 		cpp_str_buffer = cpp_str_buffer.substr(index+1,cpp_str_buffer.size());
@@ -49,10 +65,6 @@ void Parsing::profile_list_from_str(char *buffer,std::vector<Profile> *prof){
 		
 		size_t idx = name_and_score.find(delimiteur_nom);
 		Profile Player_profile{name_and_score.substr(0,idx).c_str(), atoi((name_and_score.substr(idx+1,name_and_score.size()).c_str()))};
-		/*strcpy(Player_profile.pseudo, (name_and_score.substr(0,idx)).c_str());
-		std::cout << "name_scor "<<Player_profile.pseudo<<std::endl;
-		Player_profile.score = atoi((name_and_score.substr(idx+1,name_and_score.size()).c_str()));
-		std::cout << "name_scor "<<Player_profile.score<<std::endl;*/
 		prof->push_back(Player_profile);
 	}
 
@@ -166,6 +178,114 @@ void Parsing::parsing(char* str, char* token1, char* token2) {
 		else if (i == 2 && token2) { strcpy(token2, var.c_str()); }
 		i++;
 	}
+}
+
+
+editor_settings Parsing::level_editor_from_str(char* buffer){
+
+	/*
+	L_enemyNumber_enemyHp_enemyDamage_enemySpeed_damageUp_tripleShot_lifeSteal
+	_minigun_dropRate_obstacleNumber_obstacleHp_obstacleDamage_obstacleSpeed
+	_p1Damage_p1Lives_p1ShipDesign_p2Damage_p2Lives_p2ShipDesign&
+	*/
+
+	editor_settings settings_editor{};
+	std::string delimiteur = "_";
+	std::string editor_str(buffer);
+	std::size_t indx = editor_str.find(delimiteur);
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	std::string tmp = editor_str.substr(0,indx);
+	strcpy(settings_editor._pseudo,tmp.c_str());
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+
+	indx = editor_str.find(delimiteur);
+	std::string tmp = editor_str.substr(0,indx);
+	strcpy(settings_editor._level_name,tmp.c_str());
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.enemy_number = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.enemy_hp = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.enemy_speed = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.damage_up = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.triple_shot = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.life_steal = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.minigun = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.drop_rate = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.obstacle_number = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.obstacle_hp = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.obstacle_damage = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.obstacle_speed = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.p1_damage = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.p1_lives = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.p1_ship_design = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.p2_damage = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.p2_lives = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	indx = editor_str.find(delimiteur);
+	settings_editor.p2_ship_design = std::stoi(editor_str.substr(0,indx));
+	editor_str = editor_str.substr(indx+1,editor_str.size());
+
+	return settings_editor;
+
 }
 
 
