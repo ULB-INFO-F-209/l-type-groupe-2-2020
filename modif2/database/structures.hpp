6873c2f71877473a1ba7c5d8e3f76e64a25b91bf
koadegno <vinove9@hotmail.fr>
2021-04-07 22:48:13 +0200

    delete useless

--- modif2/database/structures.hpp
+++
@@ -1,9 +0,0 @@
-#include "account.hpp"
-
-struct Profile{
-	char pseudo[20];
-	int score;
-	Profile()=default;
-	Profile(const char *p, int s){sprintf(pseudo,"%s", p); score=s;}
-	Profile(Account acc){acc.getPseudo(pseudo); score = acc.getScore();}
-};
